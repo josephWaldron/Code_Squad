239b483e26e6a9d94c91b824e13edfa260a95cc7
Joseph Waldron <waldrojo@kean.edu>
2023-03-26 18:02:08 -0400

    up to home page

---
+++ client/src/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+const LandingPage = () => {
+  return (
+    <>
+      <h1>Learn to code with engaging lessons and exciting activities!</h1>
+      <p>A beautifully designed courses to help beginners learn how to code.</p>
+      <Button variant="primary" href="/create-account">
+        Get Started
+      </Button>
+    </>
+  );
+};
+
+export default LandingPage;
---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,31 @@
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Cookies from "js-cookie";
+const N = () => {
+  const isLoggedIn = Cookies.get("userId");
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <img src="/src/assets/logo.png" alt="logo" width="60" height="60" />
+        <Navbar.Brand href="/">CodeSquad</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About / Contact</Nav.Link>
+            <NavDropdown title="Courses" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/courses/java">Java</NavDropdown.Item>
+              <NavDropdown.Item href="/courses/java">Python</NavDropdown.Item>
+            </NavDropdown>
+            {isLoggedIn ? (
+              <Nav.Link href="/profile">Profile</Nav.Link>
+            ) : (
+              <Nav.Link href="/login">Login</Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default N;
