ec4602168eb053ed50b26a1c2e826e40df923026
Joseph Waldron <waldrojo@kean.edu>
2023-04-06 13:10:09 -0400

    image fix?

--- client/src/data/courses.ts
+++ client/src/data/courses.ts
@@ -1,18 +1,22 @@
 import javaLessons from "./java";
 import pythonLessons from "./python";
 
+//import images
+import javaImg from "../images/java.png";
+import pythonImg from "../images/python.png";
+
 export default [
   {
     id: 1, //numerical id number
     name: "Java", //make sure this is capitalized
-    img: "java.png", //path to image
+    img: javaImg, //path to image
     complete: true, //is the course content finished?
     lessons: javaLessons, //array of lessons
   },
   {
     id: 2,
     name: "Python",
-    img: "python.png",
+    img: pythonImg,
     complete: false,
     lessons: pythonLessons,
   },
