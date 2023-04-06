884a828ad8048686b600463e566d92dcc45f60bc
Joseph Waldron <waldrojo@kean.edu>
2023-04-06 13:25:29 -0400

    f

--- client/src/data/courses.ts
+++ client/src/data/courses.ts
@@ -5,14 +5,14 @@ export default [
   {
     id: 1, //numerical id number
     name: "Java", //make sure this is capitalized
-    img: "./images/java.png", //path to image
+    img: "./java.png", //path to image
     complete: true, //is the course content finished?
     lessons: javaLessons, //array of lessons
   },
   {
     id: 2,
     name: "Python",
-    img: "./images/python.png",
+    img: "./python.png",
     complete: false,
     lessons: pythonLessons,
   },
