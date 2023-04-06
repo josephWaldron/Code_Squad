de9851216eb4af6746f249223f33421ef5b1791d
Joseph Waldron <waldrojo@kean.edu>
2023-04-06 14:43:11 -0400

    imgs path fixed

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import Cookies from "js-cookie";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import courses from "../data/courses";
+import logo from "../assets/logo.png";
 
 const N = () => {
   const isLoggedIn =
@@ -8,7 +9,7 @@ const N = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <img src="./logo.png" alt="logo" width="60" height="60" />
+        <img src={logo} alt="logo" width="60" height="60" />
         <Navbar.Brand href="/">CodeSquad</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
--- client/src/data/courses.ts
+++ client/src/data/courses.ts
@@ -1,18 +1,22 @@
 import javaLessons from "./java";
 import pythonLessons from "./python";
 
+//import images
+import javaImg from "../assets/java.png";
+import pythonIMG from "../assets/python.png";
+
 export default [
   {
     id: 1, //numerical id number
     name: "Java", //make sure this is capitalized
-    img: "./java.png", //path to image
+    img: javaImg, //path to image
     complete: true, //is the course content finished?
     lessons: javaLessons, //array of lessons
   },
   {
     id: 2,
     name: "Python",
-    img: "./python.png",
+    img: pythonIMG,
     complete: false,
     lessons: pythonLessons,
   },
