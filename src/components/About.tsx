22e7e06fe70393c3431c36bdc8aaabc03401c73c
Joseph Waldron <waldrojo@kean.edu>
2023-04-17 14:25:26 -0400

    Added about template

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,12 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import josephImage from "../assets/josephImg.jpg";
 
 export const About = () => {
   return (
     <>
-      <header className="py-5 text-center bg-light">
+      <header className="py-5 text-center bg-dark">
         <Container>
-          <h1 className="text-success">About / Contact</h1>
+          <h1 className="text-success">About</h1>
         </Container>
       </header>
       <section className="py-5">
@@ -13,32 +14,81 @@ export const About = () => {
           <Row className="gx-5">
             <Col md>
               <article>
-                <h2 className="mb-4">About</h2>
-
+                <h2 className="mb-4">About the site</h2>
                 <p className="lead">
                   CodeSquad is an interactive and fun website where people of
                   all ages and skill levels can learn more about coding in
                   various different languages, with hands on exercises and real
-                  world examples. Our goal is to teach as many people as possible
-                  about the extravagant world of coding. We provide a fun, easy
-                  to use interface that allows users to track their progress and
-                  see how much they have learned. All in all, CodeSquad is here
-                  to help sharpen coding skills and learn the basics of many
-                  different languages.
+                  world examples. Our goal is to teach as many people as
+                  possible about the extravagant world of coding. We provide a
+                  fun, easy to use interface that allows users to track their
+                  progress and see how much they have learned. All in all,
+                  CodeSquad is here to help sharpen coding skills and learn the
+                  basics of many different languages.
                 </p>
               </article>
             </Col>
+          </Row>
+          <h1>About the creators</h1>
+          <br />
+          {/* Copy this and add your own stuff */}
+          <Row>
             <Col md>
-              <article>
-                <h2 className="mb-4">Contact</h2>
-
-                <p className="lead">
-                  Reach out to the CodeSquad team anytime by contacting the
-                  following email address: example@kean.edu, or contact us at
-                  7328675309. Our team will get back to you as soon as possible.
-                  Thank you for learning with CodeSquad, and have a great day!
-                </p>
-              </article>
+              <img
+                width={140}
+                src={josephImage}
+                alt="Joseph"
+                className="img-fluid rounded-circle mb-4"
+              />
+              <h2 className="mb-4">Joseph Waldron</h2>
+            </Col>
+            <Col md>
+              <p className="lead">Full Stack Engineer | Kean University</p>
+              <Button
+                variant="success"
+                target="_blank"
+                href="https://github.com/josephWaldron"
+              >
+                Github
+              </Button>{" "}
+              <Button
+                variant="info"
+                href="https://portfolio-site-delta-seven.vercel.app/"
+                target="_blank"
+              >
+                Portfolio
+              </Button>
+            </Col>
+          </Row>
+          {/* Copy this and add your own stuff */}
+          <br></br>
+          <br></br>
+          <Row>
+            <Col md>
+              <img
+                width={140}
+                src={josephImage}
+                alt="Joseph"
+                className="img-fluid rounded-circle mb-4"
+              />
+              <h2 className="mb-4">Joseph Waldron</h2>
+            </Col>
+            <Col md>
+              <p className="lead">Full Stack Engineer | Kean University</p>
+              <Button
+                variant="success"
+                target="_blank"
+                href="https://github.com/josephWaldron"
+              >
+                Github
+              </Button>{" "}
+              <Button
+                variant="info"
+                href="https://portfolio-site-delta-seven.vercel.app/"
+                target="_blank"
+              >
+                Portfolio
+              </Button>
             </Col>
           </Row>
         </Container>
